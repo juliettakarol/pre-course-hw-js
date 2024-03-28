8c565ffa67f653142a878087e6ef02a987b87eb5
Julyetta Karol <julietta_karol@mail.ru>
2024-03-28 23:04:04 +0300

    added new task

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -13,17 +13,14 @@
 
 let rule = "Еще не родился тот человек, который, поставил бы цель, и не смог бы стать программистом."
 
-function ruleFunction(rule){
 switch (true) {
     case rule.length < 25:
-        console.log("Все таки нет правил без исключения"); 
-        break;
-    case rule.length === 25:
-        console.log("50 на 50"); 
-        break;
-    default:
-        console.log("У меня 100 пудов все получится")    
-}
-}
-
-ruleFunction(rule)
+      console.log('Все таки нет правил без исключения');
+      break;
+      case rule.length > 25:
+      console.log('У меня 100 пудов все получится'); 
+      break;
+      case rule.length === 25:
+      console.log('50 на 50');
+      break;
+  }

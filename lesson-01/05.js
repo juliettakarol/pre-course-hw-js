cf790c743630801b955dfbe62668f73b86b9f070
Julyetta Karol <julietta_karol@mail.ru>
2024-03-28 20:18:50 +0300

    added homework 01-05 change 1

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -14,12 +14,12 @@
 let rule = "Еще не родился тот человек, который, поставил бы цель, и не смог бы стать программистом."
 
 switch (true) {
-    case rule.length < 25:
-        console.log('"Все таки нет правил без исключения"'); 
+    case (rule.length < 25):
+        console.log("Все таки нет правил без исключения"); 
         break;
-    case rule.length > 25:
-        console.log('"У меня 100 пудов все получится"'); 
+    case (rule.length > 25):
+        console.log("У меня 100 пудов все получится"); 
         break;
-        default: 
-        console.log('"50 на 50"');       
+    default:
+        console.log("50 на 50")    
 }

c24b9a6f579ec0ed94548e975b126e8a4c7c61e7
Julyetta Karol <julietta_karol@mail.ru>
2024-04-05 16:58:38 +0300

    added new task

---
+++ lesson-02/07.js
@@ -0,0 +1,26 @@
+// Ну и вишенка на торт: раз наш парень оказался «в нужное время в нужном месте», 
+// пора бы его женить):
+// Под surname добавь married: true. И разумеется проверь, что женат только один парень… 
+// (Т.е. необходимо создать глубокую копию объекта passportMarried , и добавить в него новый ключ: значение).
+
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+
+// let passportMarried2 = JSON.parse(JSON.stringify(passportMarried))
+// passportMarried2.married = true
+
+// console.log(passportMarried)
+// console.log(passportMarried2)
+
+let passportMarried2 = { ...passportMarried,  married: true, address: { ...passportMarried.address } } 
+passportMarried2.married = true
+ 
+console.log(passportMarried); 
+console.log(passportMarried2);
